69f62f59df47933f84fdc8643eeca15f0da51729
Shiyar Daoud <daoud@axxessio.com>
2019-08-21 12:58:34 +0200

    initial start

---
+++ src/main/java/com/dt/be/poeditor/PoeditorApplication.java
@@ -0,0 +1,13 @@
+package com.dt.be.poeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dt/be/poeditor/PoeditorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dt.be.poeditor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoeditorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

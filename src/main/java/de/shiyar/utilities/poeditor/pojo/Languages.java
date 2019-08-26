c7a62bbabc5276339209cc4701daa71c8c72d2e2
Shiyar Daoud <daoud@axxessio.com>
2019-08-26 11:59:52 +0200

    add enum for languages

---
+++ src/main/java/de/shiyar/utilities/poeditor/pojo/Languages.java
@@ -0,0 +1,15 @@
+package de.shiyar.utilities.poeditor.pojo;
+
+/**
+ * @author daoud
+ * on Aug, 2019
+ */
+public enum Languages {
+    ENGLISH("en"), GERMAN("de"), TURKISH("tr");
+
+    public final String local;
+
+    Languages(String local) {
+        this.local = local;
+    }
+}
